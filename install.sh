47d53cd53b63a6ee0e31b82513a07959c1dfcd07
chlebja3 <chlebja3@fel.cvut.cz>
2025-07-03 20:04:36 +0200

    Changed structure

---
+++ install.sh
@@ -0,0 +1,8 @@
+# update ubuntu
+sudo apt update && sudo apt upgrade -y
+
+# install the basic tools
+sudo apt install -y build-essential cmake python3 python3-pip git unzip wget software-properties-common
+
+# install neovim
+sudo apt install neovim -y
